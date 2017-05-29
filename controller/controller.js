063d2b6374c4f6540c0fd34264cb9b2818e702c6
pasqualevitiello <pasqualevitiello@gmail.com>
2017-05-29 18:23:08 +0200

    typography controller changes

--- controller/controller.js
+++ controller/controller.js
@@ -10,49 +10,128 @@ document.body.insertBefore( controllerWrapper, document.body.firstChild );
  */
 // https://stackoverflow.com/questions/13856272/google-fonts-getting-a-list-of-available-fonts-through-ajax
 // https://vivekdragon.wordpress.com/2012/12/07/google-font-api-for-select/
+// https://github.com/lefoy/google-fonts/blob/master/public/js/components/GoogleFonts.js
 
 // Google API key
 var apiKey = 'AIzaSyBS2UUiahoRjToJ9g2C5Je1aHcwfFKkHPQ';
 
-// Create Typography controller HTML
-var controllerTypography = '<div class="typography-controller" style="display:none;"><select id="headings-fonts"></select><select id="body-fonts"></select></div>';
-controllerWrapper.innerHTML = controllerTypography;
-
-// Populate Typography fields
-function getFonts( fonts ) {
-	for ( var i = 0; i < fonts.items.length; i++ ) {
-		$( '#headings-fonts, #body-fonts' ).append( $('<option></option>' ).attr( 'value', fonts.items[i].family ).text( fonts.items[i].family ) );
-		//$('body').prepend( JSON.stringify( fonts.items[i] ) + '<br /><br />' );
-	}
-}
-
-
-// Create Google script node
+// Add WebFont script
 var webfontsScript = document.createElement( 'script' );
-webfontsScript.src = 'https://www.googleapis.com/webfonts/v1/webfonts?key=' + apiKey + '&callback=getFonts';
+webfontsScript.src = 'http://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js';
 document.body.appendChild( webfontsScript );
 
-// Update style
-$( '#headings-fonts' ).change( function() {
-	var id = $( '#headings-fonts option:selected' ).val();
-	$( 'h1, h2, h3, h4, h5, h6' ).css( 'font-family', id );
-});
-$( '#body-fonts' ).change( function() {
-	var id = $( '#body-fonts option:selected' ).val();
-	$( 'body' ).css( 'font-family', id );
-});
+// Add Client script
+var webfontsScript = document.createElement( 'script' );
+webfontsScript.src = 'https://apis.google.com/js/client.js?onload=loadFonts';
+document.body.appendChild( webfontsScript );
+
+// Create Typography controller HTML
+var controllerTypography = '<div class="typography-controller"><select id="headings-fonts"></select><select id="body-fonts"></select></div>';
+controllerWrapper.innerHTML = controllerTypography;
 
 
+window.googlefonts = window.googlefonts || {};
 
- WebFontConfig = {
-   google: { families: ['ABeeZee', 'Abel', 'Abril Fatface', 'Aclonica', 'Acme', 'Actor', 'Adamina', 'Advent Pro', 'Aguafina Script', 'Akronim', 'Aladin', 'Aldrich', 'Alegreya', 'Alegreya SC', 'Alex Brush', 'Alfa Slab One', 'Alice', 'Alike', 'Alike Angular', 'Allan', 'Allerta', 'Allerta Stencil', 'Allura', 'Almendra', 'Almendra Display', 'Almendra SC', 'Amarante', 'Amaranth', 'Amatic SC', 'Amethysta', 'Anaheim', 'Andada', 'Andika', 'Angkor', 'Annie Use Your Telescope', 'Anonymous Pro', 'Antic', 'Antic Didone', 'Antic Slab', 'Anton', 'Arapey', 'Arbutus', 'Arbutus Slab', 'Architects Daughter', 'Archivo Black', 'Archivo Narrow', 'Arimo', 'Arizonia', 'Armata', 'Artifika', 'Arvo', 'Asap', 'Asset', 'Astloch', 'Asul', 'Atomic Age', 'Aubrey', 'Audiowide', 'Autour One', 'Average', 'Average Sans', 'Averia Gruesa Libre', 'Averia Libre', 'Averia Sans Libre', 'Averia Serif Libre', 'Bad Script', 'Balthazar', 'Bangers', 'Basic', 'Battambang', 'Baumans', 'Bayon', 'Belgrano', 'Belleza', 'BenchNine', 'Bentham', 'Berkshire Swash', 'Bevan', 'Bigelow Rules', 'Bigshot One', 'Bilbo', 'Bilbo Swash Caps', 'Bitter', 'Black Ops One', 'Bokor', 'Bonbon', 'Boogaloo', 'Bowlby One', 'Bowlby One SC', 'Brawler', 'Bree Serif', 'Bubblegum Sans', 'Bubbler One', 'Buda', 'Buenard', 'Butcherman', 'Butterfly Kids', 'Cabin', 'Cabin Condensed', 'Cabin Sketch', 'Caesar Dressing', 'Cagliostro', 'Calligraffitti', 'Cambo', 'Candal', 'Cantarell', 'Cantata One', 'Cantora One', 'Capriola', 'Cardo', 'Carme', 'Carrois Gothic', 'Carrois Gothic SC', 'Carter One', 'Caudex','Cedarville Cursive', 'Ceviche One', 'Changa One', 'Chango', 'Chau Philomene One', 'Chela One', 'Chelsea Market', 'Chenla', 'Cherry Cream Soda', 'Cherry Swash', 'Chewy', 'Chicle', 'Chivo', 'Cinzel', 'Cinzel Decorative', 'Clicker Script', 'Coda', 'Coda Caption', 'Codystar', 'Combo', 'Comfortaa', 'Coming Soon','Concert One', 'Condiment', 'Content', 'Contrail One', 'Convergence', 'Cookie', 'Copse', 'Corben', 'Courgette', 'Cousine', 'Coustard', 'Covered By Your Grace', 'Crafty Girls', 'Creepster', 'Crete Round', 'Crimson Text', 'Croissant One', 'Crushed', 'Cuprum', 'Cutive', 'Cutive Mono']}
-  };
-  (function() {
-    var wf = document.createElement('script');
-    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
-  })();
+(function(window, document, $) {
+
+    "use strict";
+
+    googlefonts.LoadFonts = (function() {
+
+        var dropdown = $( '#headings-fonts' ),
+            preview = $( 'h1' ),
+            currentFont,
+            fonts;
+
+        function _updateHash() {
+
+            window.location.hash = currentFont.replace(/\s/g, '');
+
+        }
+
+        function _loadHash() {
+
+            var hash = window.location.hash.substring(1),
+                fontIndex;
+
+            $.each(fonts, function(index, item) {
+                if (item.family.replace(/\s/g, '') === hash) {
+                    currentFont = item.family;
+                    fontIndex = index;
+                    return false;
+                }
+            });
+
+            dropdown.find('option').eq(fontIndex).attr('selected', 'selected');
+
+            if (hash !== '') {
+                _updateFont();
+            }
+
+        }
+
+        function _display() {
+
+            $('body').addClass('is-loaded');
+
+            for (var i = 0, l = fonts.length; i < l; i++) {
+                dropdown.append('<option value="' + fonts[i].family + '">' + fonts[i].family + '</option>');
+            }
+
+            _loadHash();
+
+        }
+
+        function _updateFont() {
+
+            WebFont.load({
+                google: {
+                    families: [currentFont + ':300,400,700']
+                }
+            });
+
+            preview.css('font-family', currentFont);
+
+            _updateHash();
+        }
+
+        function _setupAPI() {
+
+            gapi.client.setApiKey(apiKey);
+
+            gapi.client.load('webfonts', 'v1', function() {
+
+                var request = gapi.client.webfonts.webfonts.list();
+                request.execute(function(resp) {
+                    fonts = resp.items;
+                    _display();
+                });
+
+            });
+
+        }
+
+        function _bindEvents() {
+
+            dropdown.on('change', function() {
+                currentFont = $(this).val().toString();
+                _updateFont();
+            });
+        }
+
+        function init() {
+            _setupAPI();
+            _bindEvents();
+        }
+
+        return {
+            init: init
+        };
+
+    })();
+
+})(window, document, $);
+
+var loadFonts = function() {
+    window.googlefonts.LoadFonts.init();
+};
